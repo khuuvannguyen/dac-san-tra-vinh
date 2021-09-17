2f0472a6189414161ddff79a92edda9eb12df072
Khưu Vạn Nguyên <67781606+khuuvannguyen@users.noreply.github.com>
2021-09-17 14:34:19 +0700

    Update a.php

--- mvc/controllers/a.php
+++ mvc/controllers/a.php
@@ -479,4 +479,29 @@ class a extends Controller
             ]);
         }
     }
+
+    function backup()
+    {
+        $backup = $this->restModel->backup();
+        date_default_timezone_set('Asia/Ho_Chi_Minh');
+		$date = date('Y_m_d');
+        $backup_file_name = "dacsantravinh" . '_backup_' . $date . '.sql';
+        $fileHandler = fopen($backup_file_name, 'w+');
+        $number_of_lines = fwrite($fileHandler, $backup);
+        fclose($fileHandler);
+
+        // Download the SQL backup file to the browser
+        header('Content-Description: File Transfer');
+        header('Content-Type: application/octet-stream');
+        header('Content-Disposition: attachment; filename=' . basename($backup_file_name));
+        header('Content-Transfer-Encoding: binary');
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate');
+        header('Pragma: public');
+        header('Content-Length: ' . filesize($backup_file_name));
+        ob_clean();
+        flush();
+        readfile($backup_file_name);
+        exec('rm ' . $backup_file_name);
+    }
 }
